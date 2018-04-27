420fe41cec18f53805f5637231df05b948dc3455
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 21:42:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,33 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table
+
+  end
+
+  def self.column_names
+
+  end
+
+  def initialize
+
+  end
+
+  def save
+
+  end
+
+  def table_names_for_insert
+
+  end
+
+  def values_for_insert
+
+  end
+
+  def column_names_for_insert
+
+  end
+
 end
